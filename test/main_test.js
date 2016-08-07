37249088fdb0972c446ffb0c97b5a9a2589f7b05
Jason <literary@yahoo.com>
2016-08-07 16:08:51 -0600

    Late but initial commit

---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+// Test Suite
+describe('Mocha', function() {
+  // Test spec (Unit Test)
+  it('should run our tests using npm', function() { //"Sanity Test"
+    expect(true).to.be.ok;
+  })
+})
